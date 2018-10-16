48d76be8c4f759a916c44176562f98428cf6358e
ghyeon0 <xcm1321@gmail.com>
2018-10-17 00:43:41 +0900

    analysis.py: bug fixed

--- analysis.py
+++ analysis.py
@@ -22,11 +22,13 @@ class Analysis:
                     self.language_dic[language] = 1
                 else:
                     self.language_dic[language] += 1
-            if soup.find("span", {"class": "disabled"}) is None:
-                print("Next Page")
-                url = str(soup.find("a", string="Next")).split("href=\"")[1].split("\" rel")[0]
-                req = requests.get(url)
-            elif soup.find("span", {"class": "disabled"}).text == "Next":
+                # print(language)
+            next_button = soup.find_all("a", {"rel": "nofollow"})
+            no_next = True
+            for each in next_button:
+                if each is not None and each.text == "Next":
+                    no_next = False
+            if no_next:
                 print("Last Page")
                 break
             else:
@@ -36,14 +38,25 @@ class Analysis:
         return self.language_dic
 
 
-def print_dic(dic):
+def print_dic(lst):
+    dic = {}
+    for l in lst:
+        for each in l:
+            if each in dic:
+                dic[each] += l[each]
+            else:
+                dic[each] = l[each]
     sorted_dic = sorted(dic, key=dic.get, reverse=True)
     for i in range(len(sorted_dic)):
         print(str(i + 1) + ":", sorted_dic[i])
 
 
 if __name__ == "__main__":
-    s = Analysis("noirbizarre")
+    import time
+    start = time.time()
+    s = Analysis("seongjoojin")
     pool = Pool(processes=4)
     temp = pool.map(s.analyze, s.url)
-    print_dic(temp[0])
+    # print(temp)
+    print_dic(temp)
+    print(time.time() - start)
